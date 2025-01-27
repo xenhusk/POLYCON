7d0f7b7043ed1a7abc8b2270903219cf11806baf
Xenhusk <desuyodavidpaul@gmail.com>
2025-01-28 04:43:18 +0800

    Deleted Node.js backend

--- backend/services/firebaseService.js
+++
@@ -1,9 +0,0 @@
-const { db } = require('../config/firebase');
-
-const saveConsultation = async (data) => {
-  const docRef = db.collection('consultations').doc();
-  await docRef.set(data);
-  return docRef.id;
-};
-
-module.exports = { saveConsultation };
