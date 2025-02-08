ccc85616cfc6e2e31988f129107ac00b02573edf
Hazy019 <kyrell0602@gmail.com>
2025-02-09 01:12:56 +0800

    Add features in Home & Login

--- frontend/my-app/src/components/Login.js
+++ frontend/my-app/src/components/Login.js
@@ -43,35 +43,32 @@ const Login = ({ onLoginSuccess, onSwitchToSignup }) => {
 
 
   return (
-      <div className="w-full h-[80vh] flex justify-center items-center">
+      <div className="w-full h-[80vh] flex justify-center items-center font-poppins">
         <form onSubmit={handleSubmit} className="flex flex-col w-full max-w-md sm:max-w-lg md:max-w-xl mx-auto">
           <img src={logo} alt="Logo" className="h-[130px] w-[130px] mx-auto"/>
           <h2 className="text-center text-lg font-bold text-[#005B98]">Login</h2>
-          <input
-            className="border-[#005B98] border-b-2 p-2 w-[76%] h-[44.59px] my-2 mx-auto focus:outline-none"
-            type="email"
-            placeholder="sample@wnu.sti.edu.ph"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required/>
-          <input
-            className="border-[#005B98] border-b-2 p-2 w-[76%] h-[44.59px] my-2 mx-auto focus:outline-none"
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required/>
+          <div class="relative z-0">
+            <input className="block py-1 px-1.5 my-2 mt-3 mx-auto w-[76%] text-base text-gray-900 bg-transparent border-0 border-b-2 border-[#005B98] appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              type="email" placeholder=" " value={email} onChange={(e) => setEmail(e.target.value)} required/>
+            <label for="Email" class="absolute text-base px-[13%] peer-focus:px-[16%] text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">
+              Email</label>
+          </div>
+          <div class="relative z-0">
+            <input className="block py-1 px-1.5 my-2 mt-3 mx-auto w-[76%] text-base text-gray-900 bg-transparent border-0 border-b-2 border-[#005B98] appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              type="password" placeholder=" " value={password} onChange={(e) => setPassword(e.target.value)} required/>
+            <label for="Password" class="absolute text-base px-[13%] peer-focus:px-[16%] text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">
+              Passowrd</label>
+          </div>
             <div className="h-[5%]">
               {message && <p className="text-center text-red-500 text-base">{message}</p>}
             </div>
-          <button
-            type="submit"
-            className="bg-[#057DCD] text-white w-[76%] h-[44.59px] rounded-lg my-2 mx-auto shadow-md hover:bg-[#54BEFF]">
+          <button type="submit" className="bg-[#057DCD] text-white w-[76%] h-[44.59px] rounded-lg my-2 mx-auto shadow-md hover:bg-[#54BEFF]">
             Login
           </button>
           <div className="border-t-2 border-[#005B98] w-[70%] my-2 mx-auto border-opacity-50">
             <p className="text-center font-light text-[0.9rem] mx-auto my-2 text-opacity-50">
-              Don't have an account? <button onClick={onSwitchToSignup} className="text-[#005B98] focus:outline-none">Sign up</button>
+              Don't have an account? <button onClick={(e) => {e.preventDefault(); onSwitchToSignup();}} 
+              className="text-[#005B98] focus:outline-none hover:underline">Sign up</button>
             </p>
           </div>
         </form>
--- frontend/my-app/tailwind.config.js
+++ frontend/my-app/tailwind.config.js
@@ -6,17 +6,35 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
       animation: {
+        'modal-fadeO': 'modalFadeOut 0.3s ease-out',
         'modal-fade': 'modalFadeIn 0.3s ease-in-out',
-        'modal-slide': 'modalSlideIn 0.3s ease-in-out',
+        'modal-slideL': 'modalSlideLeft 0.3s ease-in-out',
+        'modal-slideR': 'modalSlideRight 0.3s ease-in-out',
+        'modal-slideD': 'modalSlideDown 0.3s ease-in-out',
       },
       keyframes: {
+        modalFadeOut: {
+          '100%': { opacity: 1 },
+          '0%': { opacity: 0 },
+        },
         modalFadeIn: {
           '0%': { opacity: 0 },
           '100%': { opacity: 1 },
         },
-        modalSlideIn: {
-          '0%': { transform: 'translateY(-50px)', opacity: 0 },
+        modalSlideLeft: {
+          '0%': { transform: 'translateX(40px)', opacity: 0 },
+          '100%': { transform: 'translateX(0)', opacity: 1 },
+        },
+        modalSlideRight: {
+          '0%': { transform: 'translateX(-40px)', opacity: 0 },
+          '100%': { transform: 'translateX(0)', opacity: 1 },
+        },
+        modalSlideDown: {
+          '0%': { transform: 'translateY(20px)', opacity: 0 },
           '100%': { transform: 'translateY(0)', opacity: 1 },
         },
       }
