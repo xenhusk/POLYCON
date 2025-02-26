4b152a4b35d418600038fa0f0d68041f595c1e56
Hazy019 <kyrell0602@gmail.com>
2025-02-26 22:12:18 +0800

    Update BookingPopup.js

--- frontend/my-app/src/components/BookingPopup.js
+++ frontend/my-app/src/components/BookingPopup.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
 import BookingAppointment from './BookingAppointment';
