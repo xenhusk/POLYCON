3e395591f84dac0c08e346bc3e01bfef521e115b
Xenhusk <desuyodavidpaul@gmail.com>
2025-07-25 02:09:46 +0800

    feat: Remove semi-transparent overlay from ActionButtonsToggle component

--- frontend/my-app/src/components/ActionButtonsToggle.js
+++ frontend/my-app/src/components/ActionButtonsToggle.js
@@ -141,19 +141,6 @@ const ActionButtonsToggle = ({ children, isVisible = true }) => {
           </motion.div>
         )}
       </AnimatePresence>
-
-      {/* Semi-transparent overlay when expanded */}
-      <AnimatePresence>
-        {isExpanded && (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            className="fixed inset-0 bg-black/5 z-40"
-            onClick={() => setIsExpanded(false)}
-          />
-        )}
-      </AnimatePresence>
     </>
   );
 };
